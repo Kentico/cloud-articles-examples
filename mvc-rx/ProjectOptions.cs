0ef23bde550cca8cbcd0e1b9bf4454ff2dcf718d
Jan Lenoch <jan.lenoch@hotmail.cz>
2018-02-27 12:34:58 +0100

    Added a minimalistic async-to-rx MVC demo. CN-8

---
+++ mvc-rx/ProjectOptions.cs
@@ -0,0 +1,10 @@
+﻿using KenticoCloud.Delivery;
+
+namespace MvcRx
+{
+    public class ProjectOptions
+    {
+        public DeliveryOptions DeliveryOptions { get; set; }
+        public int CacheTimeoutSeconds { get; set; }
+    }
+}
